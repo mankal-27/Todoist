c3cfb1a285fc552f232c291899a36aeb86528a30
mankal-27 <mkalkutagi27@gmail.com>
2021-08-05 12:30:32 +0530

    added readfilesync to get user input

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
-const readlineSync = require('readline-sync');
 const dotenv = require("dotenv").config();
-const yargs = require("yargs");
+const yargs = require("yargs").argv;
+const readlineSync = require("readline-sync")
 
 const TOKEN = process.env.TOKEN;
 const URL = 'https://api.todoist.com/rest/v1/'
@@ -19,28 +19,27 @@ function getProjects(){
 }
 
 function getActiveTasks() {
+    let dataTable = ["id" , "project_id","content"]
     fetch( URL+"tasks", {
       headers: {
         Authorization: `Bearer ${TOKEN}`,
       },
     }).then((res) => res.json()
-    .then((data) => console.table(data)))
+    .then((data) => console.table(data,dataTable)))
     .catch(err => {
         console.log("Error" , err)
     })
 }
 
-function createTask(contentData, time , description,priority){
-    const task = {
-        "content" : `${contentData}`,
-        "due_string" : `${time}`,
-        "description" : `${description}`,
-        "priority" : `${priority}`,
-    };
+function createTask(){
 
+    let task = {};
+    task.content = readlineSync.question("Content : ");
+    task.due_string = readlineSync.question("Time : ");
+    task.description = readlineSync.question("Description : ");
     fetch(URL + "tasks", {
         method:'POST',
-        body : JSON.stringify(task),
+        body : JSON.stringify(data),
         headers : {
             "Content-Type": "application/json",
             Authorization: `Bearer ${TOKEN}`
@@ -52,7 +51,7 @@ function createTask(contentData, time , description,priority){
 }
 
 function closeTask(id){
-    fetch(URL + "tasks" + `${id}` + "close",{
+    fetch(URL + "tasks/" + `${id}` + "close",{
         method:'POST',
         headers:{
             Authorization: `Bearer ${TOKEN}`
@@ -65,7 +64,7 @@ function closeTask(id){
 }
 
 function deleteTask(id){
-    fetch(URL + "tasks" + `${id}`,{
+    fetch(URL + "tasks/" + `${id}`,{
         method:'POST',
         headers:{
             Authorization: `Bearer ${TOKEN}`
@@ -76,3 +75,12 @@ function deleteTask(id){
     })
 }
 
+let sendData = {
+    "content": "Drinks",
+    "due_string": "tomorrow at 2:20",
+    "due_lang": "en",
+    "priority": 4
+}
+
+createTask()
+//getActiveTasks()
