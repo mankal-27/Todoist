9a3ad326b5a3674af704b744b3e7153b0bd42460
mankal-27 <mkalkutagi27@gmail.com>
2021-08-06 11:26:19 +0530

    refractored code

--- src/index.js
+++ src/index.js
@@ -5,25 +5,22 @@ const URL = 'https://api.todoist.com/rest/v1/'
 
 const {getProjects, getActiveTasks,createTask,closeTask,deleteTask} = require('./task');
 
+const cmdControllerMapping = {
+    ls : getActiveTasks,
+    add : createTask,
+    cl : closeTask,
+    dl : deleteTask
+}
 const argv = yargs
     .command('ls' , 'Fetch all the task available')
     .command('add', 'Create/Add a task')
     .command('cl' , 'Close the task')
     .command('dl' , 'Delete the task')
-    .argv
+    .argv;
 
-    if(argv._[0] === 'ls'){
-        return getActiveTasks();
-    }
-    else if(argv._[0] === 'add'){
-        return createTask;
-    }
-    else if(argv._[0] === 'cl'){
-        return closeTask;
-    }
-    else if(argv._[0] === 'dl'){
-        return deleteTask;
-    }
-    else{
-        console.log("Bad input")
-    }
+if(argv._[0] in cmdControllerMapping){
+    cmdControllerMapping[argv._[0]]();
+}else{
+    console.log("Bad Input");
+}
+
