42943eef946593c1fdaec08c2f519dc8da1c3486
mankal-27 <mkalkutagi27@gmail.com>
2021-08-06 16:22:34 +0530

    added commands to display project task

--- src/index.js
+++ src/index.js
@@ -3,19 +3,30 @@ const yargs = require("yargs");
 const TOKEN = process.env.TOKEN;
 const URL = 'https://api.todoist.com/rest/v1/'
 
-const {getProjects, getActiveTasks,createTask,closeTask,deleteTask} = require('./task');
+const {getProjects, getProjectByID, addTaskUnderProject,closeTaskUnderProject,deleteTaskUnderProject,
+      getActiveTasks,createTask,closeTask,deleteTask} = require('./task');
 
 const cmdControllerMapping = {
     ls : getActiveTasks,
     add : createTask,
-    cl : closeTask,
-    dl : deleteTask
+    close : closeTask,
+    delete : deleteTask,
+    'lsproj' : getProjects,
+    'idproj' : getProjectByID,
+    'tsproj' : addTaskUnderProject,
+    'clproj' : closeTaskUnderProject,
+    'dlproj' : deleteTaskUnderProject,
 }
 const argv = yargs
     .command('ls' , 'Fetch all the task available')
     .command('add', 'Create/Add a task')
-    .command('cl' , 'Close the task')
-    .command('dl' , 'Delete the task')
+    .command('close' , 'Close the task')
+    .command('delete' , 'Delete the task')
+    .command('lsproj', 'Fetch all the project available')
+    .command('idproj', 'Fetch a project by id')
+    .command('tsproj', 'Add Task under Project')
+    .command('clproj', 'Close Task under Project')
+    .command('dlproj' , 'Delete Task under Project')
     .argv;
 
 if(argv._[0] in cmdControllerMapping){
