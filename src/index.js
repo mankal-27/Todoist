5f391792f6a99db418152479749a368a21a42559
mankal-27 <mkalkutagi27@gmail.com>
2021-08-06 18:19:52 +0530

    used prettier

--- src/index.js
+++ src/index.js
@@ -1,37 +1,44 @@
 const yargs = require("yargs");
 
 const TOKEN = process.env.TOKEN;
-const URL = 'https://api.todoist.com/rest/v1/'
+const URL = "https://api.todoist.com/rest/v1/";
 
-const {getProjects, getProjectByID, addTaskUnderProject,closeTaskUnderProject,deleteTaskUnderProject,
-      getActiveTasks,createTask,closeTask,deleteTask} = require('./task');
+const {
+  getProjects,
+  getProjectByID,
+  addTaskUnderProject,
+  closeTaskUnderProject,
+  deleteTaskUnderProject,
+  getActiveTasks,
+  createTask,
+  closeTask,
+  deleteTask,
+} = require("./task");
 
 const cmdControllerMapping = {
-    ls : getActiveTasks,
-    add : createTask,
-    close : closeTask,
-    delete : deleteTask,
-    'lsproj' : getProjects,
-    'idproj' : getProjectByID,
-    'tsproj' : addTaskUnderProject,
-    'clproj' : closeTaskUnderProject,
-    'dlproj' : deleteTaskUnderProject,
-}
+  ls: getActiveTasks,
+  add: createTask,
+  close: closeTask,
+  delete: deleteTask,
+  lsproj: getProjects,
+  idproj: getProjectByID,
+  tsproj: addTaskUnderProject,
+  clproj: closeTaskUnderProject,
+  dlproj: deleteTaskUnderProject,
+};
 const argv = yargs
-    .command('ls' , 'Fetch all the task available')
-    .command('add', 'Create/Add a task')
-    .command('close' , 'Close the task')
-    .command('delete' , 'Delete the task')
-    .command('lsproj', 'Fetch all the project available')
-    .command('idproj', 'Fetch a project by id')
-    .command('tsproj', 'Add Task under Project')
-    .command('clproj', 'Close Task under Project')
-    .command('dlproj' , 'Delete Task under Project')
-    .argv;
+  .command("ls", "Fetch all the task available")
+  .command("add", "Create/Add a task")
+  .command("close", "Close the task")
+  .command("delete", "Delete the task")
+  .command("lsproj", "Fetch all the project available")
+  .command("idproj", "Fetch a project by id")
+  .command("tsproj", "Add Task under Project")
+  .command("clproj", "Close Task under Project")
+  .command("dlproj", "Delete Task under Project").argv;
 
-if(argv._[0] in cmdControllerMapping){
-    cmdControllerMapping[argv._[0]]();
-}else{
-    console.log("Bad Input");
+if (argv._[0] in cmdControllerMapping) {
+  cmdControllerMapping[argv._[0]]();
+} else {
+  console.log("Bad Input");
 }
-
--- src/task.js
+++ src/task.js
@@ -1,128 +1,131 @@
-const fetch = require('node-fetch');
-const  dotenv = require("dotenv").config();
+const fetch = require("node-fetch");
+const dotenv = require("dotenv").config();
 const yargs = require("yargs");
 const readlineSync = require("readline-sync");
 
-
 const TOKEN = process.env.TOKEN;
-const URL = 'https://api.todoist.com/rest/v1/'
+const URL = "https://api.todoist.com/rest/v1/";
 
 // function for passing id
-function chooseID (){
-    let chooseID = readlineSync.question("Add id : ")
-    return chooseID
+function chooseID() {
+  let chooseID = readlineSync.question("Add id : ");
+  return chooseID;
 }
 
 function getActiveTasks() {
-    let dataTable = ["id" , "project_id","content"]
-    fetch( URL+"tasks", {
-      headers: {
-        Authorization: `Bearer ${TOKEN}`,
-      },
-    })
-    .then(res => res.json())
-    .then(data => console.table(data , dataTable))
-    .catch(err => {
-        console.log("Error at fetching task - please check " , err)
-    })
+  let dataTable = ["id", "project_id", "content"];
+  fetch(URL + "tasks", {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.table(data, dataTable))
+    .catch((err) => {
+      console.log("Error at fetching task - please check ", err);
+    });
 }
 
+function createTask(id) {
+  let tasktoAdd = {};
 
-function createTask(id){
- 
-    let tasktoAdd = {};
+  tasktoAdd.project_id = id;
+  tasktoAdd.content = readlineSync.question("Content : ");
+  tasktoAdd.due_string = readlineSync.question("Time : ");
+  tasktoAdd.description = readlineSync.question("Description : ");
 
-    tasktoAdd.project_id=id;
-    tasktoAdd.content = readlineSync.question("Content : ");
-    tasktoAdd.due_string = readlineSync.question("Time : ");
-    tasktoAdd.description = readlineSync.question("Description : ");
-    
-    fetch(URL + "tasks", {
-        method:'POST',
-        body : JSON.stringify(tasktoAdd),
-        headers : {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${TOKEN}`
-        }
-    }).then(console.log("Task Added"))
-      .catch(err => {
-        console.log("Error at creating a task - please check again" , err)
-    })
+  fetch(URL + "tasks", {
+    method: "POST",
+    body: JSON.stringify(tasktoAdd),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+    .then(console.log("Task Added"))
+    .catch((err) => {
+      console.log("Error at creating a task - please check again", err);
+    });
 }
 
-
-function closeTask(){
-    let chooseID  = readlineSync.question("Add the id to delete the task : ");
-    fetch(URL + "tasks/" + chooseID+ "/close",{
-        method:'POST',
-        headers:{
-            Authorization: `Bearer ${TOKEN}`
-        }
-    }).then(console.log("Closed Task"))
-      .catch(err => {
-        console.log("Error at closing a task - please check again" , err)
-    })
-    
+function closeTask() {
+  let chooseID = readlineSync.question("Add the id to delete the task : ");
+  fetch(URL + "tasks/" + chooseID + "/close", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+    .then(console.log("Closed Task"))
+    .catch((err) => {
+      console.log("Error at closing a task - please check again", err);
+    });
 }
 
-
-function deleteTask(){
-    let chooseID  = readlineSync.question("Add the id to delete the task : ")
-    fetch(URL + "tasks/" + chooseID,{
-        method:'DELETE',
-        headers:{
-            Authorization: `Bearer ${TOKEN}`
-        }
-    }).then(console.log("Deleted Task"))
-    .catch(err => {
-        console.log("Error" , err)
-    })
+function deleteTask() {
+  let chooseID = readlineSync.question("Add the id to delete the task : ");
+  fetch(URL + "tasks/" + chooseID, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+    .then(console.log("Deleted Task"))
+    .catch((err) => {
+      console.log("Error", err);
+    });
 }
 
-
-function getProjects(){
-    fetch(URL + "projects" ,{
-        headers: {
-            Authorization: `Bearer ${TOKEN}`,
-          },
-    })
-      .then((res) => res.json())
-      .then((data) => console.table(data))
-      .catch(err => {
-          console.log("Error" , err)
-      })
+function getProjects() {
+  fetch(URL + "projects", {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.table(data))
+    .catch((err) => {
+      console.log("Error", err);
+    });
 }
 
-
-function getProjectByID(){
-
-    let id = chooseID()
-    const URL1 = id==''?URL+`projects`:URL+`projects/${id}`;
-        fetch(URL1 ,{
-            headers: {
-                Authorization: `Bearer ${TOKEN}`,
-              },
-        })
-          .then((res) => res.json())
-          .then((data) => console.table(data))
-          .catch(err => {
-              console.log("Bad id :- check your input id again")
-          })
-    }
-
-function addTaskUnderProject(){
-     let projectID = chooseID()
-     createTask(parseInt(projectID));
+function getProjectByID() {
+  let id = chooseID();
+  const URL1 = id == "" ? URL + `projects` : URL + `projects/${id}`;
+  fetch(URL1, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.table(data))
+    .catch((err) => {
+      console.log("Bad id :- check your input id again");
+    });
 }
 
-function closeTaskUnderProject(){
-    let projectID = chooseID()
-    closeTask(parseInt(projectID));
+function addTaskUnderProject() {
+  let projectID = chooseID();
+  createTask(parseInt(projectID));
 }
 
-function deleteTaskUnderProject(){
-    let projectID = chooseID()
-    deleteTask(parseInt(projectID));
+function closeTaskUnderProject() {
+  let projectID = chooseID();
+  closeTask(parseInt(projectID));
 }
-module.exports = {getProjects, getProjectByID,addTaskUnderProject,closeTaskUnderProject,deleteTaskUnderProject,
-                 getActiveTasks,createTask,closeTask,deleteTask,}
+
+function deleteTaskUnderProject() {
+  let projectID = chooseID();
+  deleteTask(parseInt(projectID));
+}
+module.exports = {
+  getProjects,
+  getProjectByID,
+  addTaskUnderProject,
+  closeTaskUnderProject,
+  deleteTaskUnderProject,
+  getActiveTasks,
+  createTask,
+  closeTask,
+  deleteTask,
+};
